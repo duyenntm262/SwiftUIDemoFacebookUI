d46fc07479c13134c4ec00c4e98160b258fc5a41
DuyenNTM <dyndyn2602@gmail.com>
2024-04-05 13:21:09 +0700

    Initial Commit

---
+++ DemoSwiftFacebookUI/DemoSwiftFacebookUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSwiftFacebookUIApp.swift
+//  DemoSwiftFacebookUI
+//
+//  Created by Din Din on 05/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct DemoSwiftFacebookUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
